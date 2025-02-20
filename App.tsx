62cc8511ce6f3f3f27f7ed34f610f6f2e6f58be4
khushi-x6 <122792068+khushi-x6@users.noreply.github.com>
2025-02-20 12:27:33 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
+import AppNavigation from "./src/navigation/AppNavigation";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <StatusBar barStyle="light-content" backgroundColor="#340092" />
+        <NavigationContainer>
+          <AppNavigation />
+        </NavigationContainer>
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
